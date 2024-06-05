7af74febfe1cbf3b37e4186bb6f6a8f5b946e6b2
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-06-05 20:15:51 +0800

    fix 2

--- main.py
+++ main.py
@@ -12,30 +12,43 @@ BASE_URL = "https://rarserver.lostmypillow.duckdns.org/api"
 status_code = "1"
 
 
+def sendAttempt(n):
+    if n == "yes":
+        attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=True")
+        print(f"Attempt logged: {attempt_response.json()}")
+    else:
+        attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=False")
+        print(f"Attempt logged: {attempt_response.json()}")
+
+        
+def tryif():
+    try:
+        response = requests.get(f"{BASE_URL}/card/{uid}")
+        response_data = response.json()
+
+        if response.status_code == 200 and "card_val" in response_data:
+            sendAttempt("yes")
+            status_code = "9"
+
+        else:
+            sendAttempt("no")
+            status_code = "0"
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+
+
 while True:
     
     read_ser = ser.readline()
-    serm = read_ser.decode('utf-8')
+    uid = read_ser.decode('utf-8').strip()
     ser.write(status_code.encode())
-    if "Card UID: " in serm:
-        uid = serm.split("Card UID: ")[1].strip()
-        print(f"Extracted UID: {uid}")
-
-        try:
-            response = requests.get(f"{BASE_URL}/card/{uid}")
-            response_data = response.json()
-
-            if response.status_code == 200 and "card_val" in response_data:
-                attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=True")
-                print(f"Attempt logged: {attempt_response.json()}")
-                status_code = "9"
-
-            else:
-                attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=False")
-                print(f"Attempt logged: {attempt_response.json()}")
-                status_code = "0"
-
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+    print(f"Extracted UID: {uid}")
+    if uid != "":
+        tryif()
+    ser.write(status_code.encode())
+    print("sent status code " + status_code)
