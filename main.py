9486045fe75fbbf45371d93f03a71d97467367ac
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-06-05 20:35:06 +0800

    revert changes

--- main.py
+++ main.py
@@ -12,42 +12,30 @@ BASE_URL = "https://rarserver.lostmypillow.duckdns.org/api"
 status_code = "1"
 
 
-def sendAttempt(n):
-    if n == "yes":
-        attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=True")
-        print(f"Attempt logged: {attempt_response.json()}")
-    else:
-        attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=False")
-        print(f"Attempt logged: {attempt_response.json()}")
-
-
-
-
-
 while True:
-    ser.write(status_code.encode())
-    print("sent status code " + status_code)
+    
     read_ser = ser.readline()
-    uid = read_ser.decode('utf-8').strip()
-    print(f"Extracted UID: {uid}")
-    if uid != "":
+    serm = read_ser.decode('utf-8')
+    ser.write(status_code.encode())
+    if serm != "":
+        uid = serm.split("Card UID: ")[1].strip()
+        print(f"Extracted UID: {uid}")
+
         try:
             response = requests.get(f"{BASE_URL}/card/{uid}")
             response_data = response.json()
 
             if response.status_code == 200 and "card_val" in response_data:
-                sendAttempt("yes")
+                attempt_response = requests.post(f"{BASE_URL}/attempt?successornot=True")
+                print(f"Attempt logged: {attempt_response.json()}")
                 status_code = "9"
 
-
             else:
-                sendAttempt("no")
+                attempt_response = requests.post(f"{BASE_URL}/attempt?successfornot=False")
+                print(f"Attempt logged: {attempt_response.json()}")
                 status_code = "0"
 
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-        
-
-
